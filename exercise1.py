003a44556136f1416a2e1a9cf9e43a11de2918ec
8fuhst <59779937+8fuhst@users.noreply.github.com>
2022-04-11 18:03:23 +0200

    error plotting

--- exercise1.py
+++ exercise1.py
@@ -13,13 +13,22 @@ def task1():
 
 
 def stochastic_gradient_descent(iterations, dim, alpha):
+    error = []
     data_points = task1()
     theta = np.random.uniform(low=-0.5, high=0.5, size=dim)
     for z in range(0, iterations):
         for i in range(0, 100):
             for j in range(0, dim):
                 theta[j] = theta[j] + alpha*(data_points[i][1] - h(theta, data_points[i][0]))*math.pow(data_points[i][0], j)
-    return theta
+        error.append(math.sqrt(2*E(data_points, theta, 100)/100))
+    return theta, error
+
+
+def E(data_points, theta, m):
+    sum = 0
+    for i in range(0, m):
+        sum += math.pow(h(theta, data_points[i][0]) - data_points[i][1], 2)
+    return 0.5*sum
 
 
 def h(theta, x):
@@ -45,11 +54,15 @@ if __name__ == "__main__":
     DIM = 4
     ITERATIONS = 3000
     data_distr = task1()
-    result = stochastic_gradient_descent(ITERATIONS, DIM, ALPHA)
+    result, error = stochastic_gradient_descent(ITERATIONS, DIM, ALPHA)
     print(result)
     plt.scatter(data_distr[:,0], data_distr[:,1])
     poly_x, poly_y = create_polynomial(theta=result, dim=DIM)
     print(poly_y)
     plt.plot(poly_x, poly_y)
     plt.title(f"Alpha {ALPHA}, DIM {DIM}, ITERATIONS {ITERATIONS}")
+    plt.show()
+
+    plt.title("Error over time")
+    plt.plot(error, color='red')
     plt.show()
